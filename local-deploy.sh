9563a85179f0d2bb1550c0554f176ed39dbe345c
deeep08 <deeep0810@gmail.com>
2020-08-13 12:07:45 +0100

    Added lightbox in gallery page

--- local-deploy.sh
+++ local-deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -xe
 APACHE_WEB_DIR=/usr/local/var/www
 
+rm -rf $APACHE_WEB_DIR/shagunconsultants
 mkdir -p $APACHE_WEB_DIR/shagunconsultants
 cp -r web/* $APACHE_WEB_DIR/shagunconsultants/
+
+#aws s3 cp shagunconsultants/gallery.html s3://sc.deeep08.com/gallery.html --profile deeep08 --recursive
+#aws s3 cp shagunconsultants/gallery.html s3://sc.deeep08.com/gallery.html --profile deeep08
+#sudo apachectl start
